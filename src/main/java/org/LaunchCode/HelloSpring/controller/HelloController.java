0502570e8dac6a5f5e060e720be71ac4dc23be01
EthanWillCode <Aethil@yahoo.com>
2023-04-25 22:47:33 -0500

    Added some basic thymeleaf to our Hello-Spring project

---
+++ src/main/java/org/LaunchCode/HelloSpring/controller/HelloController.java
@@ -0,0 +1,36 @@
+package org.LaunchCode.HelloSpring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+
+@Controller
+public class HelloController {
+
+    @GetMapping("goodbye")
+    @ResponseBody
+    public String goodbye() {
+        return "Goodbye, Spring!";
+    }
+
+    // Handles requests of the form /hello?name=LaunchCode
+    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "hello")
+    @ResponseBody
+    public String helloWithQueryParam(@RequestParam String name) {
+        return "Hello, " + name + "!";
+    }
+
+    // Handles requests of the form /hello/LaunchCode
+    @GetMapping("hello/{name}")
+    @ResponseBody
+    public String helloWithPathParam(@PathVariable String name) {
+        return "Hello, " + name + "!";
+    }
+
+
+    @GetMapping("form")
+    public String helloForm() {
+        return "form";
+    }
+
+}
